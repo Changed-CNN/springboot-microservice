1ee85989f4fc78adf1f53c95602db2bb05ae4a3d
Melo Feng <melo_feng@Melo-MacBook-Air.local>
2021-12-17 15:32:26 -0500

    add validation for POST

--- restful-webservice/src/main/java/com/melo/restful/webservice/restfulwebservice/user/User.java
+++ restful-webservice/src/main/java/com/melo/restful/webservice/restfulwebservice/user/User.java
@@ -1,11 +1,15 @@
 package com.melo.restful.webservice.restfulwebservice.user;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 public class User {
 
     private Integer id;
+    @Size(min = 2,message = "at least two chars")
     private String name;
+    @Past
     private Date birthDate;
 
     protected User(){
