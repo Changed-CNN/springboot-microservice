b9d908370c9fc1f9a64ea8d4bf3ed205981ee31b
Melo Feng <melo_feng@Melo-MacBook-Air.local>
2021-12-17 11:02:56 -0500

    map GET one user

--- restful-webservice/src/main/java/com/melo/restful/webservice/restfulwebservice/user/UserResource.java
+++ restful-webservice/src/main/java/com/melo/restful/webservice/restfulwebservice/user/UserResource.java
@@ -2,6 +2,7 @@ package com.melo.restful.webservice.restfulwebservice.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,5 +16,9 @@ public class UserResource {
     @GetMapping(path = "/users")
     public List<User> retrieveAllUsers(){
         return service.findAll();
+    }
+    @GetMapping(path = "/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        return service.findOne(id);
     }
 }
