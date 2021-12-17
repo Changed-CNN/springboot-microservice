e9e4dc7884038baaa1a119999931f7c7dbaf96ef
Melo Feng <melo_feng@Melo-MacBook-Air.local>
2021-12-17 10:32:08 -0500

    Initialize User

--- restful-webservice/src/main/java/com/melo/restful/webservice/restfulwebservice/helloworld/HelloWorldController.java
+++ restful-webservice/src/main/java/com/melo/restful/webservice/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.melo.restful.webservice.restfulwebservice;
+package com.melo.restful.webservice.restfulwebservice.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
