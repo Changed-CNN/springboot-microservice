da2f140f9fd6a94c9a33a377c15ef41cab84831d
Melo Feng <melo_feng@Melo-MacBook-Air.local>
2021-12-17 11:51:56 -0500

    finish POST by generating location for new object

--- restful-webservice/src/main/java/com/melo/restful/webservice/restfulwebservice/user/UserResource.java
+++ restful-webservice/src/main/java/com/melo/restful/webservice/restfulwebservice/user/UserResource.java
@@ -1,8 +1,11 @@
 package com.melo.restful.webservice.restfulwebservice.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,7 +23,9 @@ public class UserResource {
         return service.findOne(id);
     }
     @PostMapping(path = "/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity<Object> createUser(@RequestBody User user){
         User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
