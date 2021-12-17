1a571422406dff88ce0e4ac9c6179a7734dbbc13
Melo Feng <melo_feng@Melo-MacBook-Air.local>
2021-12-17 10:25:40 -0500

    Initial Restful Webservice for Springboot

---
+++ restful-webservice/src/main/java/com/melo/restful/webservice/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.melo.restful.webservice.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path = "/helloworld")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
