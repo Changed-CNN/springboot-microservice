8bd6f97ac081a56a4ccde7eb327059cf7f8b87e9
Melo Feng <melo_feng@Melo-MacBook-Air.local>
2021-12-17 11:42:10 -0500

    finish initial POST

--- restful-webservice/src/main/java/com/melo/restful/webservice/restfulwebservice/user/UserResource.java
+++ restful-webservice/src/main/java/com/melo/restful/webservice/restfulwebservice/user/UserResource.java
@@ -1,9 +1,7 @@
 package com.melo.restful.webservice.restfulwebservice.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,9 @@ public class UserResource {
     @GetMapping(path = "/users/{id}")
     public User retrieveUser(@PathVariable int id){
         return service.findOne(id);
+    }
+    @PostMapping(path = "/users")
+    public void createUser(@RequestBody User user){
+        User savedUser = service.save(user);
     }
 }
