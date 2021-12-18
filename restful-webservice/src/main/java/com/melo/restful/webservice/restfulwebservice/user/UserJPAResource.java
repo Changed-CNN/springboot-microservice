03aeabacc6371ddf4edb092e17019031836e0ced
Melo Feng <melo_feng@Melo-MacBook-Air.local>
2021-12-18 13:08:55 -0500

    create POST for post

--- restful-webservice/src/main/java/com/melo/restful/webservice/restfulwebservice/user/UserJPAResource.java
+++ restful-webservice/src/main/java/com/melo/restful/webservice/restfulwebservice/user/UserJPAResource.java
@@ -20,6 +20,8 @@ public class UserJPAResource {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PostRepository postRepository;
 
     @GetMapping(path = "/jpa/users")
     public List<User> retrieveAllUsers(){
@@ -55,5 +57,25 @@ public class UserJPAResource {
             throw new UserNotFoundException("id-" + id);
         }
         return userOptional.get().getPosts();
+    }
+    @PostMapping("/jpa/users/{id}/posts")
+    public ResponseEntity<Object> createPost(@PathVariable int id, @RequestBody Post post) {
+
+        Optional<User> userOptional = userRepository.findById(id);
+
+        if(!userOptional.isPresent()) {
+            throw new UserNotFoundException("id-" + id);
+        }
+
+        User user = userOptional.get();
+
+        post.setUser(user);
+
+        postRepository.save(post);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(post.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+
     }
 }
