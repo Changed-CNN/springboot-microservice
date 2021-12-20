094c081488b9020f17ae9a7b9455af1ddcf1d789
Melo Feng <melo_feng@Melo-MacBook-Air.local>
2021-12-20 11:19:45 -0500

    update limits-service

---
+++ spring-cloud-config-server/src/test/java/com/melo/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melo.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
