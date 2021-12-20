1b50d3cd842f01f08054c69472f827f9c75d7dbe
Melo Feng <melo_feng@Melo-MacBook-Air.local>
2021-12-20 10:47:54 -0500

    create limits-service

---
+++ limits.services/src/test/java/com/melo/microservices/limits/services/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melo.microservices.limits.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
